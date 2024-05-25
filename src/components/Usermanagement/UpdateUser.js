c3fa3186a712dbf3c86101495cafb9c16d7e4498
YMinhtuan25022002 <ntmt52502@gmail.com>
2024-05-25 22:34:05 +0700

    fix UI

--- src/components/Usermanagement/UpdateUser.js
+++ src/components/Usermanagement/UpdateUser.js
@@ -42,28 +42,6 @@ const UpdateUser = ({ visible, onClose }) => {
                   </div>
                 </div>
     
-                <div class="mt-6">
-                  <label
-                    for="username"
-                    class="block text-sm font-medium leading-5 text-gray-700"
-                  >
-                    Username
-                  </label>
-                  <div class="mt-1 flex rounded-md shadow-sm">
-                    <span class="inline-flex h-10 items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 sm:text-sm">
-                      hehe.com/
-                    </span>
-                    <input
-                      id="username"
-                      name="username"
-                      type="text"
-                      required=""
-                      value="john"
-                      class="flex-1 form-input pl-3 block w-full rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5 "
-                    />
-                  </div>
-                </div>
-    
                 <div class="mb-3">
                   <div className="text-red-500 mb-3"></div>
                   <label
@@ -83,39 +61,6 @@ const UpdateUser = ({ visible, onClose }) => {
                     <option value="2">Giáo viên</option>
                     <option value="3">Học viên</option>
                   </select>
-                </div>
-    
-                <div class="mt-6">
-                  <label
-                    for="email"
-                    class="block text-sm font-medium leading-5  text-gray-700"
-                  >
-                    Email address
-                  </label>
-                  <div class="mt-1 relative rounded-md shadow-sm">
-                    <input
-                      id="email"
-                      name="email"
-                      type="email"
-                      required=""
-                      value="exam@gmail.com"
-                      class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5
-                    "
-                    />
-                    <div class="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-                      <svg
-                        class="h-5 w-5 text-red-500"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
-                          clip-rule="evenodd"
-                        ></path>
-                      </svg>
-                    </div>
-                  </div>
                 </div>
     
                 <div class="mt-6">
