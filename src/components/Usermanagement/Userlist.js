2122c2a44114dcab6a198165e814f95cba702c6c
trngochan <85632212+trngochan@users.noreply.github.com>
2024-05-26 13:03:38 +0700

    fix role

--- src/components/Usermanagement/Userlist.js
+++ src/components/Usermanagement/Userlist.js
@@ -167,7 +167,7 @@ const UserList = () => {
                                             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                                 <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                                                     <span aria-hidden className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                                    <span className="relative">{user.role}</span>
+                                                    <span className="relative">{user.roleName}</span>
                                                 </span>
                                             </td>
                                             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm text-right">
