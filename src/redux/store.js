63d6dfd0d16c156a7b1bf44c640e82ff938d5333
Vinh <vinhhgse160836@fpt.edu.vn>
2024-05-31 21:46:23 +0700

    Fix UI detail tutor

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import LoginSlice from "./auth/loginSlice";
 import UsersData from "./Usermanagement/user";
 import TutorsData from "./TutorManagement/Tutor";
+import UploadData from "./uploadFile/uploadFile";
 export default configureStore({
   reducer: {
     login: LoginSlice,
     user: UsersData,
     tutor: TutorsData,
+    upload: UploadData
   },
 });
 
