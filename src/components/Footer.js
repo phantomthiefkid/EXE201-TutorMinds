90045c63bb2f71e97814011d2d09aceef641f071
Vinh <vinhhgse160836@fpt.edu.vn>
2024-06-07 17:23:37 +0700

    Hello tutordetail

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="w-full md:w-1/4 mb-6 md:mb-0">
             <div className="flex items-center mb-4">
               <img
-                src="https://scontent.fsgn2-4.fna.fbcdn.net/v/t39.30808-6/445551860_1884650355281087_2659972058971311210_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=afA7fdo_8IUQ7kNvgGzfXrj&_nc_ht=scontent.fsgn2-4.fna&oh=00_AYCmUH7wEEo7MOax-QUL5qPfAOwHacx1dHoWZj9UvAxK6w&oe=665DFA91"
+                src="https://storage.googleapis.com/tutormind/dev%2Ftutormindslogo.png-4pBcY7.png"
                 alt="Logo"
                 className="h-10 w-10 rounded-full"
               />
