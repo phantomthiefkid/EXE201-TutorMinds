9d4f5f01c3f1a13ab9fda1574d4c84ab7b7613b7
YMinhtuan25022002 <ntmt52502@gmail.com>
2024-06-15 22:32:07 +0700

    api transaction

--- src/components/Payment/PaymentHistory.js
+++ src/components/Payment/PaymentHistory.js
@@ -1,71 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getIdOfUser } from '../../redux/auth/loginSlice';
 import axios from 'axios';
 const id = getIdOfUser();
-console.log(id);
-
-const transactions = [
-  {
-    status: 'Đang xử lý',
-    amount: '₫15,340,000',
-    date: '10 Tháng 7, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Thành công',
-    amount: '₫8,250,000',
-    date: '1 Tháng 7, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Đã hủy',
-    amount: '₫5,000,000',
-    date: '27 Tháng 6, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Đã hủy',
-    amount: '₫10,250,000',
-    date: '8 Tháng 6, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Thành công',
-    amount: '₫12,500,000',
-    date: '20 Tháng 5, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Đang xử lý',
-    amount: '₫6,750,000',
-    date: '15 Tháng 5, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Thành công',
-    amount: '₫9,300,000',
-    date: '10 Tháng 5, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Đang xử lý',
-    amount: '₫4,200,000',
-    date: '5 Tháng 5, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Thành công',
-    amount: '₫11,800,000',
-    date: '30 Tháng 4, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-  {
-    status: 'Đã hủy',
-    amount: '₫7,900,000',
-    date: '25 Tháng 4, 2022',
-    sender: 'Nguyễn Văn A',
-  },
-];
 
 const getStatusColor = (status) => {
   switch (status) {
@@ -80,17 +16,18 @@ const getStatusColor = (status) => {
   }
 };
 
-
 const PaymentHistory = () => {
+  const [transactions, setTransactions] = useState([]);
   const token = localStorage.getItem('token'); 
   const fetchData = async () => {
     try {
-      const response = await axios.get(`https://fams-management.tech/api/transaction/${id}?pageIndex=1&pageSize=10`,{
+      const response = await axios.get(`https://fams-management.tech/api/transaction/${id}?pageIndex=0&pageSize=10`,{
         headers: { 
           'accept': '*/*', 
           'Authorization': `Bearer ${token}`
         }
       });
+      setTransactions(response.data.content);
       console.log(response);
     } catch (error) {
       console.log(error);
@@ -99,57 +36,60 @@ const PaymentHistory = () => {
   
   useEffect(() => {
     fetchData();
-  }, [])
+  }, []);
 
   return (
-    <div className="bg-gray-100 mx-auto ">
-      <div className="p-4 text-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white">
-        <h1 className="text-2xl font-semibold">Lịch sử giao dịch</h1>
+    <div className="bg-gray-100 min-h-screen py-8">
+      <div className="p-6 text-center bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 text-white rounded-md shadow-lg mb-8">
+        <h1 className="text-3xl font-bold">Lịch sử giao dịch</h1>
       </div>
-      <div className="mx-auto flex justify-center">
-
-
-        <div className="w-5/6 p-4">
-          {transactions.map((transaction, index) => (
-            <div key={index} className="flex flex-col p-4 mb-4 bg-white rounded-lg shadow hover:shadow-lg">
-              <div className="flex justify-between items-center mb-2">
-                <div className="flex items-center">
-                  <div className={`w-4 h-4 rounded-full ${getStatusColor(transaction.status)}`}></div>
-                  <div className="ml-3">
-                    <h4 className="font-semibold">{transaction.status}</h4>
-                    <p className="text-gray-600">{transaction.date}</p>
-                  </div>
+      <div className="container mx-auto px-4">
+        {transactions.map((transaction, index) => (
+          <div key={index} className="flex flex-col p-6 mb-6 bg-white rounded-lg shadow hover:shadow-lg transition-shadow">
+            <div className="flex justify-between items-center mb-4">
+              <div className="flex items-center">
+                <div className={`w-4 h-4 rounded-full ${getStatusColor(transaction.status)}`}></div>
+                <div className="ml-3">
+                  <p className="text-gray-600">{transaction.createDate}</p>
                 </div>
-                <button className="ml-4 text-orange-500 hover:underline">Chi tiết</button>
               </div>
-              <div className="flex justify-between items-center">
+              <button className="ml-4 text-blue-500 hover:underline">Chi tiết</button>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <img className="w-12 h-12 rounded-full mr-4" src={transaction.owner.avatar} alt="Sender Avatar" />
                 <div>
-                  <p className="text-gray-600">Người chuyển: {transaction.sender}</p>
+                  <p className="text-gray-600">Người chuyển: {transaction.owner.fullName}</p>
                   <p className="text-gray-600">Số tiền: {transaction.amount}</p>
                 </div>
-                <div className="text-right">
-                  <p className="text-gray-600">Ngày chuyển: {transaction.date}</p>
-                  <p className="text-gray-600">{transaction.description}</p>
-                </div>
+              </div>
+              <div className="text-right">
+                <p className="text-gray-600">Ngày chuyển: {transaction.createDate}</p>
+                <p className="text-gray-600">{transaction.description}</p>
               </div>
             </div>
-          ))}
-          <div className="flex justify-between items-center mt-4">
-            <button className="text-purple-500 hover:underline">&larr; Trước</button>
-            <div className="space-x-2">
-              <button className="text-purple-500 hover:underline">1</button>
-              <button className="text-purple-500 hover:underline">2</button>
-              <button className="text-purple-500 hover:underline">3</button>
-              <button className="text-purple-500 hover:underline">4</button>
-              <button className="text-purple-500 hover:underline">...</button>
+            <div className="flex items-center mt-4">
+              <img className="w-12 h-12 rounded-full mr-4" src={transaction.receiver.avatar} alt="Receiver Avatar" />
+              <div>
+                <p className="text-gray-600">Người nhận: {transaction.receiver.fullName}</p>
+              </div>
             </div>
-            <button className="text-purple-500 hover:underline">Sau &rarr;</button>
           </div>
-          <div className="p-4 text-center text-gray-600 mt-8">
-            &copy; 2022, Bling Cloud Technologies LLC. All Rights Reserved.
+        ))}
+        <div className="flex justify-between items-center mt-8">
+          <button className="text-purple-500 hover:underline">&larr; Trước</button>
+          <div className="space-x-2">
+            <button className="text-purple-500 hover:underline">1</button>
+            <button className="text-purple-500 hover:underline">2</button>
+            <button className="text-purple-500 hover:underline">3</button>
+            <button className="text-purple-500 hover:underline">4</button>
+            <button className="text-purple-500 hover:underline">...</button>
           </div>
+          <button className="text-purple-500 hover:underline">Sau &rarr;</button>
         </div>
-
+        <div className="p-4 text-center text-gray-600 mt-8">
+          &copy; 2024, Bling Cloud Technologies LLC. All Rights Reserved.
+        </div>
       </div>
     </div>
   );
